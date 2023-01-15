034899ef1a5f30dadba1d1c3ff5c772605884a68
Daniel Houri <danielsaadia.houri@live.biu.ac.il>
2023-01-15 12:07:38 +0200

    V12

--- APi-DataBase/Models/Repositories.cs
+++ APi-DataBase/Models/Repositories.cs
@@ -7,9 +7,9 @@ namespace APi_DataBase.Models
         public int Id { get; set; }
         public string? Host_Type { get; set; }
         public string? Name_With_Owner { get; set; }
-        public int? Size { get; set; }
-        public int? Stars_count { get; set; }
-        public bool? Issues_Enabled { get; set; }
-        public int? Forks_count { get; set; }
+        public int Size { get; set; }
+        public int Stars_count { get; set; }
+        public bool Issues_Enabled { get; set; }
+        public int Forks_count { get; set; }
     }
 }
